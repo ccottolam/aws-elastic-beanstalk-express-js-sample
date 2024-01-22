eac49a9de80cf6a0b5d1cf212c3760cff476f345
ccottolam <106227734+ccottolam@users.noreply.github.com>
2024-01-22 17:25:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Fuck you PwC Rubbish Horrible teammates, princess'));
+app.get('/', (req, res) => res.send('sup9 PwC'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
